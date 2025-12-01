15311e0e51314e21adc3d3e104b027b250f28927
Ante Rota <antrot3@gmail.com>
2025-12-01 17:01:02 +0100

    Testovi

--- AbysaltoTaskTests/CartControllerTests.cs
+++ AbysaltoTaskTests/CartControllerTests.cs
@@ -1,7 +1,9 @@
 using AplicationLayer.DTOs;
 using AplicationLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Security.Claims;
 
 namespace AbysaltoTaskTests
 {
@@ -18,11 +20,26 @@ namespace AbysaltoTaskTests
             _controller = new CartController(_cartRepo.Object);
         }
 
+        //Da ima neki test
         [TestMethod]
         public async Task GetMyCart_ReturnsOk_WhenCartExists()
         {
             // Arrange
-            _cartRepo.Setup(r => r.GetCartForUserAsync(It.IsAny<int>())).ReturnsAsync(new CartResponse { Id = 1, Articles = new List<ArticleDto>() });
+            _cartRepo.Setup(r => r.GetCartForUserAsync(It.IsAny<int>()))
+                .ReturnsAsync(new CartResponse { Id = 1, Articles = new List<ArticleDto>() });
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "5")
+            };
+
+            var identity = new ClaimsIdentity(claims, "TestAuth");
+            var user = new ClaimsPrincipal(identity);
+
+            _controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
 
             // Act
             var result = await _controller.GetMyCart();
